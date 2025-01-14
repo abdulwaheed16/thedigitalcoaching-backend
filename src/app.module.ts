1135c6a9379e0a65d01e4a91b0193b84409dba32
abdulwaheed16 <abwaheed.ahmed@gmail.com>
2025-01-14 20:13:02 +0500

    back to routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common/decorators';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-// import { PrismaService } from './prisma/prisma.service';
-// import { PrismaModule } from './prisma/prisma.module';
-// import { TeachersModule } from './teachers/teachers.module';
-// import { StudentsService } from './students/students.service';
-// import { StudentsController } from './students/students.controller';
-// import { StudentsModule } from './students/students.module';
+import { PrismaService } from './prisma/prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { TeachersModule } from './teachers/teachers.module';
+import { StudentsService } from './students/students.service';
+import { StudentsController } from './students/students.controller';
+import { StudentsModule } from './students/students.module';
 
 @Module({
-  // imports: [PrismaModule, TeachersModule, StudentsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, TeachersModule, StudentsModule],
+  controllers: [AppController, StudentsController],
+  providers: [AppService, PrismaService, StudentsService],
 })
 export class AppModule {}
